bebc696db794b8f529cc47b28d753247057270f0
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-01-25 14:39:59 +0900

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+<p align="center">
+    <h1 align="center">pdf_thumbnail.inc.php</h1>
+    <p align="center">A plugin that inserts PDF thumbnail images for PukiWiki.</p>
+</p>
---
+++ pdf_thumbnail.inc.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * PDF Thumbnail Plugin
+ *
+ * @author     nozomu <https://nozomu.dev>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.html    GPLv3
+ * @link       https://github.com/nozomu-y/pdf_thumbnail.inc.php
+ * @version    $Id: pdf_thumbnail.inc.php, v 1.0 2023-01-25 nozomu $
+ * @package    plugin
+ */
+
+// resolution of the thumbnail
+define('PLUGIN_PDF_THUMBNAIL_RESOLUTION', 72); // default: 72
+
+// value of the target attribute of anchor tag
+define('PLUGIN_PDF_THUMBNAIL_ANCHOR_TARGET', '_blank');
+
+// style of the thumbnail image
+define('PLUGIN_PDF_THUMBNAIL_STYLE', 'width: 300px; max-width: 100%;');
+
+// whether to use cache
+define('PLUGIN_PDF_THUMBNAIL_CACHE', TRUE);
+
+// cache directory
+define('PLUGIN_PDF_THUMBNAIL_CACHEDIR', CACHE_DIR . 'pdf_thumbnail/');
+
+// whether to disable external file
+define('PLUGIN_PDF_THUMBNAIL_DISABLE_EXTERNAL_FILE', TRUE);
+
+// Usage
+define('PLUGIN_PDF_THUMBNAIL_USAGE', "([pagename/]attached-file-name|url)");
+
+function url_to_base64($url)
+{
+    $imagick = new Imagick();
+    $imagick->setResolution(PLUGIN_PDF_THUMBNAIL_RESOLUTION, PLUGIN_PDF_THUMBNAIL_RESOLUTION);
+    $imagick->readImage($url);
+    $imagick->setIteratorIndex(0);
+    $imagick->setImageBackgroundColor('#ffffff');
+    $imagick->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
+    $imagick->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
+    $imagick->setImageFormat('png');
+    $base64 = base64_encode($imagick);
+    return $base64;
+}
+
+function plugin_pdf_thumbnail_convert()
+{
+    global $vars;
+
+    // get the arguments given to the plugin
+    $args = func_get_args();
+    if (count($args) != 1) {
+        return htmlsc('<p>#pdf_thumbnail(): Usage:' . PLUGIN_PDF_THUMBNAIL_USAGE . "</p>\n");
+    }
+
+    // uri/url of the file
+    $uri = $args[0];
+    $is_attachment = !is_url($uri);
+    if (!$is_attachment && PLUGIN_PDF_THUMBNAIL_DISABLE_EXTERNAL_FILE) {
+        return htmlsc('Use of external file is disabled: ' . $uri);
+    }
+
+    // current page
+    $page_name = isset($vars['page']) ? $vars['page'] : '';
+
+    if ($is_attachment) {
+        $attachment_name = $uri;
+        if (!is_dir(UPLOAD_DIR)) {
+            return 'Upload dir does not exist';
+        }
+        $matches = array();
+
+        // whether the link contains page name
+        if (preg_match('#^(.+)/([^/]+)$#', $attachment_name, $matches)) {
+            if ($matches[1] == '.' || $matches[1] == '..') {
+                $matches[1] .= '/'; // Restore relative paths
+            }
+            $attachment_name = $matches[2];
+            $page_name = get_fullname(strip_bracket($matches[1]), $page_name); // strip is a compat
+            $file_path = UPLOAD_DIR . encode($page_name) . '_' . encode($attachment_name);
+            $is_file = is_file($file_path);
+        } else {
+            // Simple single argument
+            $file_path = UPLOAD_DIR . encode($page_name) . '_' . encode($attachment_name);
+            $is_file = is_file($file_path);
+        }
+        if (!$is_file) {
+            return htmlsc('File not found: "' . $attachment_name . '" at page "' . $page_name . '"');
+        }
+        $url = $file_path . '[0]'; // use the first page only
+        $anchor_link = get_base_uri() . '?plugin=attach' . '&amp;refer=' . rawurlencode($page_name) . '&amp;openfile=' . rawurlencode($attachment_name);
+    } else {
+        $url = htmlsc($uri);
+        $anchor_link = $url;
+    }
+
+    // use cache for attachments
+    if ($is_attachment) {
+        // create cache dir if it does not exist
+        if (!file_exists(PLUGIN_PDF_THUMBNAIL_CACHEDIR) && PLUGIN_PDF_THUMBNAIL_CACHE) {
+            mkdir(PLUGIN_PDF_THUMBNAIL_CACHEDIR);
+        }
+
+        $cache_file = PLUGIN_PDF_THUMBNAIL_CACHEDIR . md5($file_path . '_' . PLUGIN_PDF_THUMBNAIL_RESOLUTION);
+        // check if cache was generated after the attachment was uploaded
+        if (file_exists($cache_file) && (filemtime($cache_file) > filemtime($file_path)) && PLUGIN_PDF_THUMBNAIL_CACHE) {
+            $base64 = file_get_contents($cache_file);
+        } else {
+            // create base64 and save as cache
+            if (!extension_loaded('imagick')) {
+                return 'Imagick not installed';
+            }
+            try {
+                $base64 = url_to_base64($url);
+            } catch (Exception $e) {
+                return $e->getMessage();
+            }
+            // save cache
+            if (PLUGIN_PDF_THUMBNAIL_CACHE) {
+                file_put_contents($cache_file, $base64, LOCK_EX);
+            }
+        }
+    } else {
+        if (!extension_loaded('imagick')) {
+            return 'Imagick not installed';
+        }
+        try {
+            $base64 = url_to_base64($url);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    return '<a href="' . $anchor_link . '" target="' . PLUGIN_PDF_THUMBNAIL_ANCHOR_TARGET . '"><img src="data:image/png;base64,' . $base64 . '" style="' . PLUGIN_PDF_THUMBNAIL_STYLE . '"></a>';
+}
